7902f226c5cbce07f22be1913e42c86674a0af70
ELGCSystem <elgcsystem@gmail.com>
2024-04-05 21:40:28 -0300

    Modification in userModel, the emailToken now is false in unique value

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -4,13 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, trim: true },
     email: { type: String, trim: true, unique: true },
-    emailToken: {
-      type: String,
-      trim: true,
-      required: false,
-      unique: true,
-      sparse: true,
-    },
+    emailToken: { type: String, trim: true, required: false, unique: false },
     password: { type: String, trim: true },
     verified: { type: Boolean, default: false },
   },
