d6f5db000b5bfebb39fbb2b78ef88ec6976b9202
ELGCSystem <elgcsystem@gmail.com>
2024-04-05 21:13:44 -0300

    Modification in userModel

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -4,7 +4,13 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, trim: true },
     email: { type: String, trim: true, unique: true },
-    emailToken: { type: String, trim: true, required: false, unique: true },
+    emailToken: {
+      type: String,
+      trim: true,
+      required: false,
+      unique: true,
+      sparse: true,
+    }, // Usar sparse index
     password: { type: String, trim: true },
     verified: { type: Boolean, default: false },
   },
